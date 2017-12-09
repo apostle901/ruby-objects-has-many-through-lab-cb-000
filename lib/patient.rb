8a64c052d93248021596b00ea7c88778e26351e7
Jawann <jawann@gmail.com>
2017-12-09 03:00:23 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,13 @@
 class Patient
-  attr_accessor :name
+  attr_accessor :name, :appointments
 
   def initialize(name)
     @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    self.appointments << appointment
+    appointment.patient = self
   end
 end
