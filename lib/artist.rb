3e3752491fc3856a334ef36f924ffd71269c8ac9
Jawann <jawann@gmail.com>
2017-12-09 02:12:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,10 +4,12 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @genres = []
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
+    @genres << self.genre
   end
 end
